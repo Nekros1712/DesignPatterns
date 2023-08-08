76d518b39b8f6d162db91b64280d27bebb6bb5f2
Samprit JC <sampritchaurasiya07@gmail.com>
2023-08-08 12:44:22 +0000

    Add Composite pattern solution

--- DesignPatterns/main/java/net/media/training/designpattern/composite/File.java
+++ DesignPatterns/main/java/net/media/training/designpattern/composite/File.java
@@ -7,18 +7,30 @@ package net.media.training.designpattern.composite;
  * Time: 9:18:04 PM
  * To change this template use pre_refactoring.File | Settings | pre_refactoring.File Templates.
  */
-public class File {
+public class File implements DataNode{
     private final String name;
     private int size;
-    private Directory parent;
+    private DataNode parent;
 
     public File(String name, int size) {
         this.name = name;
         this.size = size;
     }
 
-    public void setParent(Directory parent) {
+    public void setParent(DataNode parent) {
         this.parent = parent;
+    }
+
+    public DataNode getData() {
+        return null;
+    }
+
+    public void remove(DataNode dataEntity) {
+        this.getParent().remove(this);
+    }
+
+    public boolean exists(String name) {
+        return true;
     }
 
     public String getName() {
